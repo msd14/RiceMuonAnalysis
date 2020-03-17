c2d75179f3fb299190ab3f552e8c5918b74ccb8d
Matthew <msd14@rice.edu>
2020-03-17 11:24:58 -0500

    Update GENSIM config file for EMTF++ era,configurations,global tag.

--- Pythia8_CP5_HadronizerFilter_13TeV_cfi_GEN_SIM.py
+++ Pythia8_CP5_HadronizerFilter_13TeV_cfi_GEN_SIM.py
@@ -7,7 +7,8 @@ import FWCore.ParameterSet.Config as cms
 
 from Configuration.StandardSequences.Eras import eras
 
-process = cms.Process('SIM',eras.Run2_2018)
+#process = cms.Process('SIM',eras.Run2_2018)
+process = cms.Process('REPR',eras.Phase2C8_trigger)
 
 # import of standard configurations
 process.load('Configuration.StandardSequences.Services_cff')
@@ -15,8 +16,8 @@ process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.load('Configuration.EventContent.EventContent_cff')
 process.load('SimGeneral.MixingModule.mixNoPU_cfi')
-process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
-process.load('Configuration.StandardSequences.GeometrySimDB_cff')
+#process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+#process.load('Configuration.StandardSequences.GeometrySimDB_cff')
 process.load('Configuration.StandardSequences.MagneticField_cff')
 process.load('Configuration.StandardSequences.Generator_cff')
 process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic25ns13TeVEarly2018Collision_cfi')
@@ -24,6 +25,10 @@ process.load('GeneratorInterface.Core.genFilterSummary_cff')
 process.load('Configuration.StandardSequences.SimIdeal_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.load('Configuration.Geometry.GeometryExtended2023D41Reco_cff')
+process.load('Configuration.Geometry.GeometryExtended2023D41_cff')
+process.load('Configuration.StandardSequences.SimL1Emulator_cff')
+
 
 process.maxEvents = cms.untracked.PSet(
         input = cms.untracked.int32(10)
@@ -33,7 +38,7 @@ process.maxEvents = cms.untracked.PSet(
 process.source = cms.Source("LHESource",
     dropDescendantsOfDroppedBranches = cms.untracked.bool(False),
     #fileNames = cms.untracked.vstring('file:MSSMD_100k_50mm_1.lhe', 'file:MSSMD_100k_50mm_2.lhe', 'file:MSSMD_100k_50mm_3.lhe', 'file:MSSMD_100k_50mm_4.lhe', 'file:MSSMD_100k_50mm_5.lhe', 'file:MSSMD_100k_50mm_6.lhe', 'file:MSSMD_100k_50mm_7.lhe', 'file:MSSMD_100k_50mm_8.lhe', 'file:MSSMD_100k_50mm_9.lhe', 'file:MSSMD_100k_50mm_10.lhe'),
-    fileNames = cms.untracked.vstring('file:MSSMD_100k_50mm_1.lhe'),
+    fileNames = cms.untracked.vstring('file:/uscms/home/mdecaro/nobackup/Dimuon/CMSSW_10_2_3/src/lhe_files/MSSMD_100k_50mm_1.lhe'),
     inputCommands = cms.untracked.vstring(
         'keep *', 
         'drop LHEXMLStringProduct_*_*_*'
@@ -69,10 +74,11 @@ process.FEVTDEBUGHLToutput = cms.OutputModule("PoolOutputModule",
 # Additional output definition
 
 # Other statements
-process.XMLFromDBSource.label = cms.string("Extended")
+#process.XMLFromDBSource.label = cms.string("Extended")
 process.genstepfilter.triggerConditions=cms.vstring("generation_step")
 from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, '102X_upgrade2018_realistic_v9', '')
+#process.GlobalTag = GlobalTag(process.GlobalTag, '102X_upgrade2018_realistic_v9', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, '103X_upgrade2023_realistic_v2', '')
 
 process.generator = cms.EDFilter("Pythia8HadronizerFilter",
     PythiaParameters = cms.PSet(
